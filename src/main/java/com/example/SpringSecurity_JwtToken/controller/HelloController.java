5a54ed6da8cf293dfbe872e08d982e57a2315ed8
Pratapk45 <pratapkachare45@gmail.com>
2025-06-26 13:29:29 +0530

    first commit

--- src/main/java/com/example/SpringSecurity_JwtToken/controller/HelloController.java
+++ src/main/java/com/example/SpringSecurity_JwtToken/controller/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String firstApi(HttpServletRequest request){
-        return "Welcome to Telusko "+request.getSession().getId();
+        return "Welcome to Pratap "+request.getSession().getId();
     }
 }
